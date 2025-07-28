fe24e447a94222b7769364ca6b108f044695811a
Vraj Makwana <vrajm@zignuts.com>
2025-07-28 11:49:03 +0530

    Add All Route to practice.txt File

--- main.py
+++ main.py
@@ -3,38 +3,3 @@ from typing import Optional
 from pydantic import BaseModel
 app =FastAPI()
 
-@app.get("/")
-async def hello_world():
-    return {
-        "msg" : "Hello world"
-    }   
-
-@app.get("/greet/{username}")
-async def greet(username,q:Optional[str]=None,data=Body()):
-    print(data)
-    print(q)
-    print(data['Hello'])
-    return {
-        "msg" : f"Good Morning {username}"
-    }
-
-class User(BaseModel):
-    name: str
-    age: int
-    email: str
-
-@app.post("/user")
-async def create_user(user: User):
-    return {
-        "msg": "User created successfully",
-        "user": user
-    }
-
-@app.get("/user/get-header")
-async def get_user_header(
-    accept:str = Header(None)
-):
-    print(accept)
-    return {
-        "Data" : "Done"
-    }
