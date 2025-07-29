df4bc32523a19950cbffa71caf45576f82c2bca9
Vraj Makwana <vrajm@zignuts.com>
2025-07-29 18:01:15 +0530

    Adding Database Configuration Files

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from src.books.routes import bookRouter
 from src.utils.Response import Response
+from contextlib import asynccontextmanager
 
 # Run Command : fastapi ./src
 
-app = FastAPI()
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print("Server is Starting...")
+    yield # this is for differentiate starting of app and ending of app
+    print("Server has been Stopped")
+
+app = FastAPI(
+    lifespan=life_span
+)
 
 app.include_router(bookRouter,prefix="/book")
 
---
+++ src/config.py
@@ -0,0 +1,16 @@
+# here we make special class that inherits BaseSettings Class that allow us to Read .env File Variables.
+
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+
+# This Class's Instance give you all Environment Variable Access
+class Settings(BaseSettings):
+    DATABASE_URL : str
+    model_config = SettingsConfigDict(
+        env_file="./env",
+        extra="ignore" 
+    )
+
+
+# Instance of Settings Class that will Provide all .env Variables
+Config = Settings()
